01ce681d73cc4f694c4bde15b1d27ed87618babd
piezga <piezga@gmail.com>
2023-03-01 21:16:14 +0100

    cambiato path

--- timestorytest.py
+++ timestorytest.py
@@ -12,19 +12,24 @@ my = np.load('D:\\xy\\data\\sigma_4.00\\simulation_test\\L_64\\magnetization\\my
 
 
 
-T1 = T[0]
-T2 = T[27]
+# T1 = T[0]
+# T2 = T[27]
 
-mx1 = mx[0,:]
-mx2 = mx[27,:]
+# mx1 = mx[0,:]
+# mx2 = mx[27,:]
 
-t = np.arange(0,len(mx1),1)
+# t = np.arange(0,len(mx1),1)
 
 
-fig,(ax1,ax2) = plt.subplots(2)
-ax1.plot(t,mx1)
-ax2.plot(t,mx2)
-ax1.grid(True)
-ax2.grid(True)
-ax1.set(xlabel = 'T = 0,64', ylabel = 'mx')
-ax1.set(xlabel = 'T = 0,81', ylabel = 'mx')
+# fig,(ax1,ax2) = plt.subplots(2)
+# ax1.plot(t,mx1)
+# ax2.plot(t,mx2)
+# ax1.grid(True)
+# ax2.grid(True)
+# ax1.set(xlabel = 'T = 0,64', ylabel = 'mx')
+# ax1.set(xlabel = 'T = 0,81', ylabel = 'mx')
+
+
+
+X = np.arange(0,64,1)
+Y = X
