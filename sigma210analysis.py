05543e251188772cefc45abfd7ec2be3e4a08a1f
piezga <piezga@gmail.com>
2023-03-01 12:41:47 +0100

    lavoro con riccardo 1marzo

--- sigma210analysis.py
+++ sigma210analysis.py
@@ -2,12 +2,60 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-temps = np.load('data/sigma_2.10/simulation_sigma210/L_256/magnetization/T.npy', allow_pickle=True)
-mx = np.load('data/sigma_2.10/simulation_sigma210/L_256/magnetization/mx_test_0.npy', allow_pickle=True)
-my = np.load('data/sigma_2.10/simulation_sigma210/L_256/magnetization/my_test_0.npy', allow_pickle=True)
-
+plt.rcParams['figure.dpi'] = 150
 
 
-fig, (ax1,ax2) = plt.subfigure(2)
-ax1 = plt.p
+Ls = [16,32,64,128,256]
+test = 2
+
+for L in Ls:    
+    temps = np.load(f'data/sigma_2.10/simulation_sigma210/L_{L}/magnetization/T.npy', allow_pickle=True)
+    mx = np.load(f'data/sigma_2.10/simulation_sigma210/L_{L}/magnetization/mx_test_{test}.npy', allow_pickle=True)[:,50000:]
+    my = np.load(f'data/sigma_2.10/simulation_sigma210/L_{L}/magnetization/my_test_{test}.npy', allow_pickle=True)[:,50000:]
+    T = np.load(f'/media/piezga/4C58C05B517A20D6/xy/data/sigma_2.10/simulation_sigma210/L_{L}/magnetization/T.npy', allow_pickle=True)
+    
+    
+    files = os.listdir(f'/media/piezga/4C58C05B517A20D6/xy/data/sigma_2.10/simulation_sigma210/L_{L}/test_{test}/last_configuration/')
+    
+    # for file in files[:-10]:
+        
+    
+    #     config = np.fromfile(f'/media/piezga/4C58C05B517A20D6/xy/data/sigma_2.10/simulation_sigma210/L_{L}/test_{test}/last_configuration/{file}', dtype = 'float64').reshape((L,L))
+        
+        
+    #     #plt.figure(1)
+    #     #plt.plot(mx[3])
+    #     fig, ax = plt.subplots(1)
+    #     ax.set_title(file)
+    #     scale = 200
+        
+    #     # Create a grid of x and y values    
+    #     x, y = np.meshgrid(np.arange(config.shape[1]), np.arange(config.shape[0]))    
+    #     # Print the shapes of the input arrays      # Define a color map    
+    #     cmap = plt.get_cmap('twilight')   
+    #     #colors = cyclic_colors(config+np.pi)   
+    #     colors = config
+    #     # Plot black arrows with the config    
+    #     ax.quiver(x, y, np.cos(config), np.sin(config), color='black',scale=scale,              width = 10**-3,              headwidth = 2.5,              headaxislength = 2,              headlength = 2              )    
+    #     fig.colorbar(ax.pcolormesh(x, y,     colors,                                cmap=cmap, alpha=0.3, shading='auto'),                ax = ax)
+        
+        
+    binder = []    
+    m = []
+    for t in range(len(T)):
+        m2 = mx[t]**2 + my[t]**2
+        m4 = m2**2 
+        binder.append(2 - m4.mean()/m2.mean()**2)
+        m.append(m2.mean())
+    plt.figure(1)
+    plt.plot(T,binder,label=str(L))
+    plt.legend()
+    plt.figure(2)
+    plt.plot(T,m,label=str(L))
+    plt.legend()
+    
+    
+    
+    
---
+++ sigma210l128test2.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+plt.rcParams['figure.dpi'] = 150
+
+
+L = 128
+test = 2
+
+temps = np.load(f'data/sigma_2.10/simulation_sigma210/L_{L}/magnetization/T.npy', allow_pickle=True)
+mx = np.load(f'data/sigma_2.10/simulation_sigma210/L_{L}/magnetization/mx_test_{test}.npy', allow_pickle=True)[:,50000:]
+my = np.load(f'data/sigma_2.10/simulation_sigma210/L_{L}/magnetization/my_test_{test}.npy', allow_pickle=True)[:,50000:]
+T = np.load(f'/media/piezga/4C58C05B517A20D6/xy/data/sigma_2.10/simulation_sigma210/L_{L}/magnetization/T.npy', allow_pickle=True)
+
+
+files = os.listdir(f'/media/piezga/4C58C05B517A20D6/xy/data/sigma_2.10/simulation_sigma210/L_{L}/test_{test}/last_configuration/')
+
+for file in files[:-10]:
+    
+
+    config = np.fromfile(f'/media/piezga/4C58C05B517A20D6/xy/data/sigma_2.10/simulation_sigma210/L_{L}/test_{test}/last_configuration/{file}', dtype = 'float64').reshape((L,L))
+    
+    
+    #plt.figure(1)
+    #plt.plot(mx[3])
+    fig, ax = plt.subplots(1)
+    ax.set_title(file)
+    scale = 200
+    
+    # Create a grid of x and y values    
+    x, y = np.meshgrid(np.arange(config.shape[1]), np.arange(config.shape[0]))    
+    # Print the shapes of the input arrays      # Define a color map    
+    cmap = plt.get_cmap('twilight')   
+    #colors = cyclic_colors(config+np.pi)   
+    colors = config
+    # Plot black arrows with the config    
+    ax.quiver(x, y, np.cos(config), np.sin(config), color='black',scale=scale,              width = 10**-3,              headwidth = 2.5,              headaxislength = 2,              headlength = 2              )    
+    fig.colorbar(ax.pcolormesh(x, y,     colors,                                cmap=cmap, alpha=0.3, shading='auto'),                ax = ax)
+
